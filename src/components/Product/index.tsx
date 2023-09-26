04a359a9428bf6a2f3fbb627918c85a968708eb5
Bruno Gallotte <brunogallotte.sci@gmail.com>
2023-09-26 09:15:33 -0300

    first commit

---
+++ src/components/Product/index.tsx
@@ -0,0 +1,13 @@
+import Tag from "../Tag"
+
+const Product = () => (
+  <div>
+    <img src={} />
+    <h3>Nome do jogo</h3>
+    <Tag>Categoria</Tag>
+    <Tag>Windows</Tag>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit at nihil iure distinctio ipsam cupiditate repellendus quidem eius. Impedit consequuntur deserunt vel ipsa voluptatibus repellat minima omnis ex consequatur asperiores!
+    </p>
+  </div>
+)
