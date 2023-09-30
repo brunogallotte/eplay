c07caff5b3760cb1fe50c1f25f0b6de6ea5f7771
Bruno Gallotte <brunogallotte.sci@gmail.com>
2023-09-30 10:15:45 -0300

    Insertion of the product page, hero creation, section about the game and more details

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom'
 
 import Categories from './Pages/Categories'
 import Home from './Pages/Home'
+import Product from './Pages/Product'
 
 const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/categories" element={<Categories />} />
+    <Route path="/product/:id" element={<Product />} />
   </Routes>
 )
 
