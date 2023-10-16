57609b917c5ac2c22fb73a7fcbd8788bf5bcfb91
Bruno Gallotte <brunogallotte.sci@gmail.com>
2023-10-16 14:52:27 -0300

    Added checkout page, form and validations

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom'
 import Categories from './Pages/Categories'
 import Home from './Pages/Home'
 import Product from './Pages/Product'
+import Checkout from './Pages/Checkout'
 
 const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/categories" element={<Categories />} />
     <Route path="/product/:id" element={<Product />} />
+    <Route path="/checkout" element={<Checkout />} />
   </Routes>
 )
 
